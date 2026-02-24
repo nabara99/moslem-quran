4c8d3c4e1e17d831dc339c1477555d41f6d5f93a
Nabara99 <rtaufiq212@gmail.com>
2026-02-24 22:19:46 +0800

    remove access background location

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -67,5 +67,6 @@ flutter {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 }
